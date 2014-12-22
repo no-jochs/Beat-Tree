a44ca89977dd6fcdc06ff938a7f88431c0151f9a
John Ochs <johnochs@me.com>
2014-12-22 03:25:35 -0800

    Removing db/migrate because there are no migrations

--- db/migrate/20141222105702_create_tracks.rb
+++
@@ -1,8 +0,0 @@
-class CreateTracks < ActiveRecord::Migration
-  def change
-    create_table :tracks do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20141222105715_create_artists.rb
+++
@@ -1,8 +0,0 @@
-class CreateArtists < ActiveRecord::Migration
-  def change
-    create_table :artists do |t|
-
-      t.timestamps
-    end
-  end
-end
