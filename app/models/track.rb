0eb720debab5ac3c1d6e5eae9b810f2cf145b51e
John Ochs <johnochs@me.com>
2014-12-22 03:00:58 -0800

    Adding neo4j code to track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,17 @@
-class Track < ActiveRecord::Base
+class Track
+  include Neo4j::ActiveNode
+  property :title
+  property :text, default: "blah blah"
+  property :score, type: Integer, default: 0
+  
+  validates :title, presence: true
+  validates :score, numericality: { only_integer: true }
+  
+  index :title
+  
+  before_save do
+    self.score = score * 100
+  end
+  
+  has_n(:comments).to(Comment)
 end
