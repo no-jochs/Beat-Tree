4a454eacae9505739854d5f3521140238e082366
John Ochs <johnochs@me.com>
2014-12-22 03:02:29 -0800

    Adding neo4j code and relationships to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
-class Comment < ActiveRecord::Base
+class Comment
+  include Neo4j::ActiveNode
+  property :title
+  property :text
+  
+  index :title
+  has_one(:track).from(Track, :comments)
 end
