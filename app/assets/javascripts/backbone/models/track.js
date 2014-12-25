0d3a49f865d2f859999eeb36945cc75f3ffd6139
John Ochs <johnochs@me.com>
2014-12-25 10:23:36 -0800

    Adding a collection for sampled_tracks, sampled_by_tracks, and a adding code to parse to deal with these things

--- app/assets/javascripts/backbone/models/track.js
+++ app/assets/javascripts/backbone/models/track.js
@@ -1,10 +1,32 @@
 BT.Models.Track = Backbone.Model.extend({
+	initialize: function () {
+		this.sampled_tracks = new BT.Collections.Tracks;
+		this.sampled_by_tracks = new BT.Collections.Tracks;
+	},
 	urlRoot: "api/tracks",
 	parse: function (respJSON) {
-		if (respJSON.track) {
-			return respJSON.track;
-		} else {
-			return respJSON;
+		if (respJSON.sampled_tracks) {
+			var st = [];
+			_(respJSON.sampled_tracks).each( function (track) {
+				var model = new BT.Models.Track(track);
+				st.push(model);
+			});
+			debugger
+			this.sampled_tracks.set(st);
+			delete respJSON.sampled_tracks;
 		}
-	}
+		if (respJSON.sampled_by) {
+			var st = [];
+			_(respJSON.sampled_by).each( function (track) {
+				var model = new BT.Models.Track(track);
+				st.push(model);
+			});
+			this.sampled_by_tracks.set(st);
+			delete respJSON.sampled_by;
+		}
+		if (respJSON.track) {
+			delete respJSON.track;
+		}
+		return respJSON;
+	},
 })
