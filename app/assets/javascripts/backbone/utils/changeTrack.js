c5196e6311fa108bfcb8ce86d7a2eb932366cf52
John Ochs <johnochs@me.com>
2014-12-28 13:24:27 -0800

    Track queue is functioning properly

--- app/assets/javascripts/backbone/utils/changeTrack.js
+++ app/assets/javascripts/backbone/utils/changeTrack.js
@@ -5,18 +5,27 @@ BT.Utils.changePlayerTrack = function (spot_track_id) {
 		'<iframe src="https://embed.spotify.com/?uri=spotify:track:' + spot_track_id + '"' + ' width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
 	);
 	
-	var trackQueue = $('.track-queue-container');
-	trackQueue.append(
-		'<div class="track-queue-item-container" data-track-id="' + spot_track_id + '">' + 
-		'<iframe src="https://embed.spotify.com/?uri=spotify:track:' + spot_track_id + '"' + ' width="250" height="80" frameborder="0" allowtransparency="true"></iframe>' +
-		'<button type="button" class="btn btn-danger" onclick="BT.Utils.cleanUpButton(' + '\'' + spot_track_id + '\'' + ')"><span class="glyphicon glyphicon-remove-sign"></span></button>' + 
-		'</div>'
-	);
+	//Only add track to the track queue if the track queue is open
 	
-	$('button')
+	if ($('#collapse-track-queue').hasClass('collapse in')) {
+		var trackQueue = $('.track-queue-container');
+		trackQueue.append(
+			'<div class="track-queue-item-container" data-track-id="' + spot_track_id + '">' + 
+			'<iframe src="https://embed.spotify.com/?uri=spotify:track:' + spot_track_id + '"' + ' width="250" height="80" frameborder="0" allowtransparency="true"></iframe>' +
+			'<button type="button" class="btn-danger track-queue-delete" onclick="BT.Utils.cleanUpButton(' + '\'' + spot_track_id + '\'' + ')"><span class="glyphicon glyphicon-remove-sign"></span></button>' + 
+			'</div>'
+		);
+	}
 	
 }
 
 BT.Utils.cleanUpButton = function (id) {
-	debugger
+	tracksInQueue = $.find('.track-queue-item-container');
+	
+	_(tracksInQueue).each( function (trackContainer) {
+		if ($(trackContainer).data('track-id') === id) {
+			$(trackContainer).remove();
+		}
+	});
 }
+
