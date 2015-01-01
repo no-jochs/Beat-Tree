f133255820b8a7ba0c337bca704f3592f0f6d9ee
John Ochs <johnochs@me.com>
2014-12-31 19:55:12 -0800

    Returning type, startNode, and endNode from the custom CYPHER query AS other things because parens screw things up.  Also, renaming query_type for predecessors and progeny query explicitly.

--- app/controllers/api/neojsons_controller.rb
+++ app/controllers/api/neojsons_controller.rb
@@ -1,9 +1,9 @@
 class Api::NeojsonsController < ApplicationController
   
   def show
-    if params[:query_type] == 'trackshow'
+    if params[:query_type] == 'predandprog'
       # json2 = Neo4j::Session.query("MATCH (a:Track)")
-      json = Neo4j::Session.query("MATCH (n:Track)-[r]-(n2:Track) WHERE n.track_spotify_id = '#{params[:node_id]}' RETURN type(r), startNode(r), endNode(r)")
+      json = Neo4j::Session.query("MATCH (n:Track)-[r]-(n2:Track) WHERE n.track_spotify_id = '#{params[:node_id]}' RETURN type(r) AS type, startNode(r) AS startNode, endNode(r) as endNode")
       render json: json, status: :ok
     else
       render json: ['Not Implemented'], status: :not_implemented
