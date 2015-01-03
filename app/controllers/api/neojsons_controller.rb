597fdee315d1dee2dcd0cb4015c006d9a91c58dd
John Ochs <johnochs@me.com>
2015-01-02 18:03:57 -0800

    Adding cypher queries for the track show D3 views

--- app/controllers/api/neojsons_controller.rb
+++ app/controllers/api/neojsons_controller.rb
@@ -2,11 +2,16 @@ class Api::NeojsonsController < ApplicationController
   
   def show
     if params[:query_type] == 'predandprog'
-      # json2 = Neo4j::Session.query("MATCH (a:Track)")
       json = Neo4j::Session.query("MATCH (n:Track)-[r]-(n2:Track) WHERE n.track_spotify_id = '#{params[:node_id]}' RETURN type(r) AS type, startNode(r) AS startNode, endNode(r) as endNode")
       render json: json, status: :ok
+    elsif params[:query_type] == 'origins'
+      json = Neo4j::Session.query("MATCH p=(n:Track)-[r*]->(n2) WHERE n.track_spotify_id = '#{params[:node_id]}' AND NOT (n2)-[]->()  UNWIND r AS rel RETURN startNode(rel) AS startNode, endNode(rel) AS endNode, type(rel) AS type")
+      render json: json, status: :ok
+    elsif params[:query_type] == 'inspirations'
+      json = Neo4j::Session.query("MATCH p=(n:Track)<-[r*]-(n2) WHERE n.track_spotify_id = '#{params[:node_id]}' AND NOT (n2)<-[]-()  UNWIND r AS rel RETURN startNode(rel) AS startNode, endNode(rel) AS endNode, type(rel) AS type")
+      render json: json, status: :ok
     else
-      render json: ['Not Implemented'], status: :not_implemented
+      render json: "Not Implemented", status: :not_implemented
     end
   end
   
