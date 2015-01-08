43b8cf409374a8227440a7d0c08dded3713eddf4
John Ochs <johnochs@me.com>
2015-01-07 19:50:53 -0800

    Adding auth methods for loggin in, logging out, and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  
+  helper_method :logged_in?, :current_user
+  
+  def login!(user)
+    session[:session_token] = user.reset_token!
+    @current_user = user
+  end
+  
+  def logout!
+    current_user.reset_token!
+    session[:session_token] = nil
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+    @current_user
+  end
+  
 end
