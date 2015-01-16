f7163667f3694ad7167e3e6ae4f91ee9d1ad28e6
John Ochs <johnochs@me.com>
2015-01-16 15:12:43 -0800

    Redirection upon completion of save

--- app/assets/javascripts/backbone/views/relationship/editRelationship.js
+++ app/assets/javascripts/backbone/views/relationship/editRelationship.js
@@ -19,6 +19,13 @@ BT.Views.EditRelationship = Backbone.CompositeView.extend({
 		debugger
 		
 		var attrs = $('#relationship-update-form').serializeJSON();
-		this.model.save(attrs);
+		this.model.save(attrs, {
+			success: function (model, response, options) {
+				Backbone.history.navigate('#relationship/' + this.startNode.get('track_spotify_id') + 
+						'?type=' + this.type + '&endNodeId=' + this.endNode.get('track_spotify_id'),
+						{ trigger: true}
+				);
+			}
+		});
 	}
-})
+});
