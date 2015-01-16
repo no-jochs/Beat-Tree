9904a2753163f216fe920415fa0311cf7ef60992
John Ochs <johnochs@me.com>
2015-01-15 21:44:44 -0800

    Adding a event listener for clicking the update relationship button and associated action for that

--- app/assets/javascripts/backbone/views/relationship/relationship.js
+++ app/assets/javascripts/backbone/views/relationship/relationship.js
@@ -1,11 +1,22 @@
 BT.Views.Relationship = Backbone.CompositeView.extend({
 	initialize: function () {},
 	
+	events: {
+		"click button#update-relationship": "updateRelationship"
+	},
+	
 	template: JST['backbone/templates/relationship/relationship'],
 	
 	render: function () {
 		var renderedContent = this.template({ relationship: this.model });
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	updateRelationship: function () {
+		Backbone.history.navigate( "#relationship/edit/" + this.type +
+									"/" + this.startNodeId + "/" + this.endNodeId,
+									{ trigger: true}
+								);
 	}
 })
