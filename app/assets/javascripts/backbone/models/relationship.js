065ed7d2c9582d5d601f9d880e599d97407d16e5
John Ochs <johnochs@me.com>
2015-01-16 14:30:12 -0800

    Adding back the http://www.beat-tree.com to the base of the url?

--- app/assets/javascripts/backbone/models/relationship.js
+++ app/assets/javascripts/backbone/models/relationship.js
@@ -6,7 +6,7 @@ BT.Models.Relationship = Backbone.Model.extend({
 	},
 	
 	url: function () {
-		return "api/relationships/" + this.startNodeId +"?type=" + 
+		return "http://www.beat-tree.com/api/relationships/" + this.startNodeId +"?type=" + 
 				this.type + "&endNodeId=" + this.endNodeId
 	},
 	
