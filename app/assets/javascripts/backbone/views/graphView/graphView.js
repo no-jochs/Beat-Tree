4e1363f9d78e61e449682d692f0b0bf4346c841f
John Ochs <johnochs@me.com>
2015-01-18 22:58:16 -0800

    Adding #mostSampled ajax call and events for all the buttons

--- app/assets/javascripts/backbone/views/graphView/graphView.js
+++ app/assets/javascripts/backbone/views/graphView/graphView.js
@@ -3,11 +3,39 @@ BT.Views.GraphView = Backbone.CompositeView.extend({
 	
 	template: JST['backbone/templates/graphView/graphView'],
 	
-	events: {},
+	events: {
+		"click button#most-sampled": "mostSampled",
+		"click button#most-covered": "mostCovered",
+		"click button#most-remixed": "mostRemixed",
+		"click button#lots-o-samples": "lotsOfSamples",
+		"click button#lone-nodes": "loneNodes",
+		"click button#mother-o-drums": "motherOfDrums",
+		"click button#popularity": "popularity",
+		"click button#track-subgraph": "trackSubgraph",
+		"click button#path-search": "pathSearch"
+	},
 	
 	render: function () {
 		var renderedContent = this.template();
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	mostSampled: function () {
+		var that = this;
+		if (!this.mostSampledData) {
+			$.ajax({
+				$.ajax({
+					type: "GET",
+					url: "http://www.beat-tree.com/api/neojson?query_type=most-sampled"
+				}).done( function (jsonResp) {
+					that.fillGraph(jsonResp);
+				});
+			})
+		}
+	},
+	
+	fillGraph: function (jsonData) {
+		
 	}
 })
