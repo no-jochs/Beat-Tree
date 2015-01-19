d7b36d7cc58d0621b0bc7b67443390d62fe62bb5
John Ochs <johnochs@me.com>
2015-01-19 00:51:23 -0800

    Adding #mostCovered and #mostRemixed

--- app/assets/javascripts/backbone/views/graphView/graphView.js
+++ app/assets/javascripts/backbone/views/graphView/graphView.js
@@ -39,6 +39,42 @@ BT.Views.GraphView = Backbone.CompositeView.extend({
 		}
 	},
 	
+	mostCovered: function () {
+		var that = this;
+		
+		var title = "Most Covered Tracks",
+			info = "Showing the top 5 most covered tracks in the BeatTree database along with the tracks that cover them.";
+		if (!this.mostCoveredData) {
+			$.ajax({
+				type: "GET",
+				url: "api/neojson?query_type=most-covered"
+			}).done( function (jsonResp) {
+				that.mostCoveredData = jsonResp;
+				that.fillGraph(that.mostCoveredData, title, info);
+			});
+		} else {
+			this.fillGraph(this.mostCoveredData, title, info);
+		}
+	},
+	
+	mostRemixed: function () {
+		var that = this;
+		
+		var title = "Most Remixed Tracks",
+			info = "Showing the top 5 most remixed tracks in the BeatTree database along with the tracks that cover them.";
+		if (!this.mostRemixedData) {
+			$.ajax({
+				type: "GET",
+				url: "api/neojson?query_type=most-remixed"
+			}).done( function (jsonResp) {
+				that.mostRemixedData = jsonResp;
+				that.fillGraph(that.mostRemixedData, title, info);
+			});
+		} else {
+			this.fillGraph(this.mostRemixedData, title, info);
+		}
+	},
+	
 	fillGraph: function (data, title, info) {
 		BT.Utils.GVD3(this, data, title, info);
 	}
