9749fe600cfe41e5695c8752bfa2359c940e473d
John Ochs <johnochs@me.com>
2015-01-20 04:21:06 -0800

    Another login fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil if user.nil?
-    user.valid_password?(password) ? return user : return nil
+    user.valid_password?(password) ? user : nil
   end
   
   def reset_token!
